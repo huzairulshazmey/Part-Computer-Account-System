90b6a5a830a49f5e40011309fa6ad7a799dcd479
huzairul shazmey <huzairulshazmey00@gmail.com>
2024-06-25 09:02:18 +0800

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Accounting system
 
 Accounting system to track expenses and generate receipts for them
+
+
+#Install
+1. npm install
+2. npm start
--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8ETr2UZwp9sxE2Ki8q5Dy1_4f6mOjC4s",
-  authDomain: "myaestic-4291a.firebaseapp.com",
-  projectId: "myaestic-4291a",
-  storageBucket: "myaestic-4291a.appspot.com",
-  messagingSenderId: "139897060758",
-  appId: "1:139897060758:web:6bc7d898e89529889d31b7",
-  measurementId: "G-M2BSBFXZ72"
+  apiKey: "Your ApiKey",
+  authDomain: "Your authDomain",
+  projectId: "Your projectId",
+  storageBucket: "Your storageBucket",
+  messagingSenderId: "Your messagingSenderId",
+  appId: "Your appId",
+  measurementId: "Your MeasurementId"
 };
 
 const app = initializeApp(firebaseConfig);
